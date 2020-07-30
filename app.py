53c1e3eaf0903079e9986dc549514750a1aa1e51
Francis McGuire <mcguirefrancis@gmail.com>
2020-07-29 20:26:01 -0400

    app.py2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -12,22 +12,22 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
 
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    int_features = [str(x) for x in request.form.values()]
+    #final_features = [np.array(int_features)]
+    #prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    output = int_features
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Top 5 orgs $ {}'.format(output))
 
-@app.route('/results',methods=['POST'])
-def results():
+# @app.route('/results',methods=['POST'])
+# def results():
 
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
+#     output = prediction[0]
+#     return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
